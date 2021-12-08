1699e666dc0d290653b2ba8a9512ac386fe74277
Kevin <kevin_mendez30@outlook.com>
2021-12-07 21:46:08 -0600

    fix: 🐛 (CORS) Added fastify-cors

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const serverless = require('serverless-http');
 const { routers } = require('./routes/post');
 const Fastify = require('fastify');
 
+
 const fastify = Fastify({
   logger: true
 });
-
+fastify.register(require('fastify-cors'), {})
 fastify.register(routers);
 
 module.exports.handler = serverless(fastify);
